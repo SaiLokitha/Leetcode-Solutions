e1cfede682df672918b2f08f463291804791c4e7
SaiLokitha <bsailokitha@gmail.com>
2025-11-16 11:12:57 +0530

    Time: 1 ms (54.7%), Space: 42.3 MB (14.63%) - LeetHub

--- 3995-gcd-of-odd-and-even-sums/3995-gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/3995-gcd-of-odd-and-even-sums.java
@@ -1,19 +1,20 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-     int odd=n*n;
-     int even=n*(n+1);
-     int max=0;
-     if(n==1) return n;
-     for(int i=1;i<odd;i++)
-        {
-            if(odd%i==0 && even%i==0)
-            {
-                if(i>max)
-                {
-                    max=i;
-                }
-            }
-        }
-        return max;
+    //  int odd=n*n;
+    //  int even=n*(n+1);
+    //  int max=0;
+    //  if(n==1) return n;
+    //  for(int i=1;i<odd;i++)
+    //     {
+    //         if(odd%i==0 && even%i==0)
+    //         {
+    //             if(i>max)
+    //             {
+    //                 max=i;
+    //             }
+    //         }
+    //     }
+    //     return max;
+    return n;
     }
 }
